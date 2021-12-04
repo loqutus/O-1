7bb5c54c64d18fd8f5ac3210978dfcfa11e29ec6
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2021-12-04 21:01:27 +0100

    commit

--- pkg/restapi/main.go
+++ pkg/restapi/main.go
@@ -3,27 +3,16 @@ package httpserver
 import (
 	"net/http"
 
-	"github.com/loqutus/O-1/pkg/restapi/controller"
-
 	chi "github.com/go-chi/chi/v5"
 )
 
 func Start() {
 	r := chi.NewRouter()
 
-	c := controller.NewController()
-
-	v1 := r.Group("/api/v1")
-	{
-		files := v1.Group("/accounts")
-		{
-			files.GET(":id", c.GetFile)
-			files.GET("", c.ListFiles)
-			files.POST("", c.AddFile)
-			files.DELETE(":id", c.DeleteFile)
-		}
-		//...
-	}
+	r.Get(":id", GetFile)
+	r.Get("", ListFiles)
+	r.Post("", AddFile)
+	r.Delete(":id", DeleteFile)
 
 	http.ListenAndServe(":6969", r)
 }
