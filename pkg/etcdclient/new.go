65dcd4921901fa00c83a62bbc2f145e8da7e81f2
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2022-01-28 22:15:04 +0100

    commit

--- pkg/etcdclient/new.go
+++ pkg/etcdclient/new.go
@@ -4,6 +4,7 @@ import (
 	"github.com/loqutus/O-1/pkg/types"
 	"github.com/sirupsen/logrus"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
 )
 
 func New() (*clientv3.Client, error) {
@@ -11,6 +12,7 @@ func New() (*clientv3.Client, error) {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{"http://" + types.Server.ETCDHost + ":" + types.Server.ETCDPort},
 		DialTimeout: types.Server.Timeout,
+		DialOptions: []grpc.DialOption{grpc.WithBlock()},
 	})
 	return cli, err
 }
