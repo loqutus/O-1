9f4e3088a1b7693c5a8a9ab91ba0841e3057eeff
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2022-02-12 00:45:25 +0100

    commit

--- pkg/client/upload_test.go
+++ pkg/client/upload_test.go
@@ -3,11 +3,14 @@ package client
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
+	"github.com/loqutus/O-1/pkg/sha256"
 	fileSHA256 "github.com/loqutus/O-1/pkg/sha256"
 	"github.com/loqutus/O-1/pkg/types"
+	"github.com/sirupsen/logrus"
 )
 
 func TestUpload(t *testing.T) {
@@ -61,4 +64,32 @@ func upload(t *testing.T) {
 	if err == nil {
 		t.Fatal("file should not exist")
 	}
+}
+
+func Write(fileNameWithPath string, data []byte) (int64, string, error) {
+	logrus.Println("Writing file:", fileNameWithPath)
+	fileName := strings.TrimPrefix(fileNameWithPath, types.Server.LocalDir)
+	fileNameSplit := strings.Split(fileName, "/")
+	fileNameSplitLen := len(fileNameSplit)
+	fileNameDir := strings.Join(fileNameSplit[:fileNameSplitLen-1], "/")
+	fullPathDir := types.Server.LocalDir + "/" + fileNameDir
+	logrus.Println("fullPathDir:", fullPathDir)
+	err := EnsureDir(fullPathDir)
+	if err != nil {
+		return 0, "", err
+	}
+	err = os.WriteFile(fileNameWithPath, data, 0644)
+	if err != nil {
+		return 0, "", err
+	}
+	fi, err := os.Stat(fileNameWithPath)
+	if err != nil {
+		return 0, "", err
+	}
+	fileSize := fi.Size()
+	fileHash, err := sha256.GetFileSHA256(fileNameWithPath)
+	if err != nil {
+		return 0, "", err
+	}
+	return fileSize, fileHash, nil
 }
