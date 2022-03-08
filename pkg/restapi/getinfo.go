94a429865830f7200f334a0fdc642216f809b8a2
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2022-03-08 23:43:49 +0100

    commit

--- pkg/restapi/getinfo.go
+++ pkg/restapi/getinfo.go
@@ -11,5 +11,4 @@ func GetInfo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(types.Info)
-	return
 }
